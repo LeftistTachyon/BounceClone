fb9e14a488d05aa7aa8606502a8e3eabf033d7e3
Zaman <K1328441@students.katyisd.org>
2019-03-06 12:27:37 -0600

    Sync

---
+++ bounce.js
@@ -0,0 +1,76 @@
+var animate = window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    function(callback) { window.setTimeout(callback, 1000/60) };
+
+var canvas = document.createElement('canvas');
+var width = 1500;
+var height = 800;
+canvas.width = width;
+canvas.height = height;
+var context = canvas.getContext('2d');
+
+window.onload = function() {
+  document.body.appendChild(canvas);
+  animate(step);
+};
+
+var step = function() {
+  update();
+  render();
+  animate(step);
+};
+
+var update = function() {
+};
+
+var render = function() {
+  context.fillStyle = "#00bfff";
+  context.fillRect(0, 0, width, height);
+};
+
+function Ball(x, y) {
+  this.x = x;
+  this.y = y;
+  this.x_speed = 0;
+  this.y_speed = 3;
+  this.radius = 50;
+}
+
+Ball.prototype.render = function() {
+  context.beginPath();
+  context.arc(this.x, this.y, this.radius, 2 * Math.PI, false);
+  context.fillStyle = "#FF0000";
+  context.fill();
+};
+
+var ball = new Ball(200, 300);
+
+var render = function() {
+  context.fillStyle = "#00bfff";
+  context.fillRect(0, 0, width, height);
+  ball.render();
+};
+
+var keysDown = {};
+
+window.addEventListener("keydown", function(event) {
+  keysDown[event.keyCode] = true;
+});
+
+window.addEventListener("keyup", function(event) {
+  delete keysDown[event.keyCode];
+});
+
+Ball.prototype.update = function() {
+  for(var key in keysDown) {
+    var value = Number(key);
+    if(value == 37) { // left arrow
+      this.paddle.move(-4, 0);
+    } else if (value == 39) { // right arrow
+      this.paddle.move(4, 0);
+    } else {
+      this.paddle.move(0, 0);
+    }
+  }
+};
